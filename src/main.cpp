22dcae1375daa8cd6af7dbd55168820e80570bec
TenViki <viktorkomarek28@gmail.com>
2024-11-28 13:30:06 +0100

    :sparkles: Adds deep sleep

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include <NimBLEDevice.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BEACON_ID "findflow_beacon"
+#define DEEP_SLEEP_TIME 10000000
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  setCpuFrequencyMhz(80);
+  NimBLEDevice::init(BEACON_ID);
+  esp_bt_sleep_enable();
+
+  NimBLEServer *pServer = NimBLEDevice::createServer();
+  NimBLEAdvertisementData oAdvertisementData = NimBLEAdvertisementData();
+  oAdvertisementData.setName(BEACON_ID);
+  oAdvertisementData.setFlags(0x04); // BR_EDR_NOT_SUPPORTED 0x04
+
+  NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->setAdvertisementData(oAdvertisementData);
+  pAdvertising->setAdvertisementType(BLE_GAP_CONN_MODE_NON);
+  pAdvertising->setMinInterval(32); // 100ms (160 * 0.625ms)
+  pAdvertising->setMaxInterval(64); // 150ms (240 * 0.625ms)
+
+  pAdvertising->start();
+  delay(200);
+  pAdvertising->stop();
+
+  setCpuFrequencyMhz(10);
+
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_XTAL, ESP_PD_OPTION_OFF);
+  esp_deep_sleep(DEEP_SLEEP_TIME);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // empty loop
 }
